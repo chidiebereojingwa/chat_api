0a7f30913369ce408c17e26ba416abc0e0d03eb9
chidiebereojingwa <ojingwaoji@gmail.com>
2022-07-26 20:27:45 +0100

    Add user route

---
+++ backend/routes/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+// Login route
+
+router.post('./login', () => {})
+
+
+// signup route
+
+router.post('./signup', () => {})
+
+
+module.exports = router
